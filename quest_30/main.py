3fc85d9dbaf7d975e6f4da57990783c72a8c5314
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-10-07 16:45:02 +0000

    quest_30

--- quest_30/main.py
+++ quest_30/main.py
@@ -3,3 +3,20 @@
 # Tenha um método público registrar(mensagem) que cria uma string formatada como "{timestamp} - {mensagem}" e a adiciona à lista privada. Use datetime.datetime.now().
 # Tenha um método público exibir_logs() que imprime cada mensagem da lista em uma nova linha. Instancie a classe, registre duas mensagens e, ao final, exiba os logs.
 
+import datetime
+class Log:
+    def __init__(self):
+        self.__mensagens = []
+
+    def registrar(self, mensagem):
+        timestamp = datetime.datetime.now()
+        log_formatado = f"{timestamp} - {mensagem}"
+        self.__mensagens.append(log_formatado)
+
+    def exibir_logs(self):
+        for mensagem in self.__mensagens:
+          print(mensagem)
+log = Log()
+log.registrar("Primeira mensagem")
+log.registrar("Segunda mensagem")
+log.exibir_logs()
